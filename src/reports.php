33716dda1ecaf57e3c9c49e6beb1a94aba6258ef
Igor Inkovskiy <Inkovskiy@gmail.com>
2017-08-14 17:20:50 +0300

    fix(reports.php) fix plainReport()

--- src/reports.php
+++ src/reports.php
@@ -58,7 +58,8 @@ function plainReport(array $ast)
             $pathToNode = implode('.', $parents);
             switch ($node['type']) {
                 case 'nested':
-                    $acc = array_merge($acc, $iter($node['children'], "{$pathToNode}"));
+                    $parents[] = $node['node'];
+                    $acc = array_merge($acc, $iter($node['children'], $parents));
                     break;
                 case 'added':
                     if (is_array($node['to'])) {
